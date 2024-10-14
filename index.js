4091f7576b63ba9d6a79017ea1c7fbc4b157f6a3
josephdeborah <josephdeborah873@gmail.com>
2024-10-14 09:25:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let welcome1 = alert(" Welcome to DEB BMI calc ");
+let welcome = prompt(" What is your name ");
+alert("hello " + welcome + " welcome" + " to DEB BMI calc ");
+
+let height = prompt(" To proceed tell me your height ");
+height **= 2;
+let weight = prompt(" Tell me your weight in gramme ");
+weight /= 1000;
+
+let result = weight * height;
+alert(welcome + " your BMI is " + result + "mh-2");
+alert(" Thanks for choosing DEB BMI calc. ");
+alert(" Looking forward to see you agan ");
+
+
+
+  
